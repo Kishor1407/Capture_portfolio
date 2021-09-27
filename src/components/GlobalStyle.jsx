aadfb550e5e44fb832b13648450e214ba2eaedd6
Kiran Kumar Hegge <kiranhegge@gmail.com>
2021-09-28 00:34:16 +0530

    6.Font Style of GlobalStyle

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -10,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
 
 body{
     background: #1b1b1b;
+    font-family: 'Inter', sans-serif;
 }
 
 button{
@@ -21,6 +22,8 @@ button{
     background: transparent;
     color: white;
     transition: all 0.5s ease;
+    font-family: 'Inter', sans-serif;
+    ${'' /* font-family: 'Lobster', cursive; */}
     &:hover{
         background-color: #23d997 ;
         color: white;
@@ -28,7 +31,7 @@ button{
 }
 h2{
     font-weight: lighter;
-    font-size: 4rem;
+    font-size: 3.5rem;
 }
 h3{
     color: white;
@@ -46,7 +49,7 @@ span{
 p{
     padding: 3rem 0rem;
     color: #ccc;
-    font-size: 1.4rem; 
+    font-size: 1.2rem; 
     line-height: 150%;
 }
 `;
