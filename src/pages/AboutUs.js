7bd8785a7484d02889fa9d4d3837a34fd3a5f3c9
Kiran Kumar Hegge <kiranhegge@gmail.com>
2021-09-29 01:26:21 +0530

    13.Page Transitions

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,14 +2,18 @@ import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
 import FaqSection from "../components/FaqSection";
+//animation
+import { motion } from "framer-motion"; 
+import {pageAnimation} from "../animation";
+
 
 const AboutUs = () => {
     return( 
-     <div>
-       <AboutSection />
-       <ServicesSection />
-       <FaqSection />
-     </div>
+      <motion.div exit="exit" variants={pageAnimation} initial="hidden" animate="show">
+          <AboutSection />
+          <ServicesSection />
+          <FaqSection />
+       </motion.div>
     )
 }
 
