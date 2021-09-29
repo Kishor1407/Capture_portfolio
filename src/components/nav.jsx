f7d8497792f582412078173499ebd6de3b58b186
Kiran Kumar Hegge <kiranhegge@gmail.com>
2021-09-30 03:44:22 +0530

    24.Nav Line Animation

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useLocation } from "react-router";
 
 const Nav = () => {
+    const {pathname} = useLocation();
     return (
       <StyledNav>
         <h1>
@@ -12,13 +15,17 @@ const Nav = () => {
         </h1>
         <ul>
             <li>
-                <Link to="/">1. About Us</Link>
+                <Link to="/">1. About Us</Link>          
+                <Line transition={{duration:0.75}} initial={{width:"0%"}} animate={{width: pathname === "/" ? "65%" : "0%"}} />
             </li>
             <li>
                 <Link to="/work">2. Our Work</Link>
+                <Line transition={{duration:0.75}} initial={{width:"0%"}} animate={{width: pathname === "/work" ? "65%" : "0%"}}  />
             </li>
             <li>
                 <Link to="/contact">3. Contact Us</Link>
+                <Line transition={{duration:0.75}} initial={{width:"0%"}} animate={{width: pathname === "/contact" ? "62%" : "0%"}} />
+
             </li>
         </ul>
       </StyledNav>
@@ -72,5 +79,20 @@ const StyledNav = styled.nav`
     }
    }
 `;
+
+const Line = styled(motion.div)`
+  height: 0.3rem;
+  background: #23d997;
+  width: 0%;
+  position: absolute;
+  bottom: -80%;
+  left: 42%;
+  @media (max-width: 1300px){
+      left: 45%;
+  }
+  @media (max-width: 800px){
+      left: 20%;
+  }
+`
 
 export default Nav;
