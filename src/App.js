073789f036a4ef6d3098dc4e429d9489df21bafd
Kiran Kumar Hegge <kiranhegge@gmail.com>
2022-07-24 20:29:52 +0530

    minor fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/nav";
 import OurWork from "./pages/OurWork";
-import ContactUs from "./pages/Contactus";
+import ContactUs from "./pages/ContactUs";
 import MovieDetail from "./pages/MovieDetail";
 //Router
 import {Switch, Route, useLocation} from "react-router-dom"
